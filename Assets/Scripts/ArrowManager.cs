a1bbd5d1939f8d633c8c118bda4f83b7163585eb
Adam <przywarty.it@gmail.com>
2017-11-23 21:48:15 +0100

    Add files via upload

---
+++ Assets/Scripts/ArrowManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowManager : MonoBehaviour
+{
+    private GameObject currentArrow;
+
+    public static ArrowManager Instance;
+
+    public OVRInput.Controller controller;
+    public GameObject controler;
+    public GameObject arrowPrefab;
+    public GameObject LastArrow;
+    public GameObject arrowStartPoint;
+    public TextMesh debug;
+
+    private bool hasArrow = false;
+
+    private bool isAttached = false;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+    }
+
+    void OnDestroy()
+    {
+        if (Instance == this)
+            Instance = null;
+    }
+
+    void Update()
+    {
+        debug.text = controler.transform.rotation.x.ToString() +
+                     "\n" + controler.transform.rotation.y.ToString() +
+                     "\n" + controler.transform.rotation.z.ToString();
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) > 0)
+        {
+            if (!hasArrow)
+            {
+                AttachArrow();
+            } else if (isAttached) {
+                Fire();
+            }
+        }
+        else ThrowArrow();
+    }
+
+    public void ThrowArrow()
+    {
+        LastArrow = currentArrow;
+        currentArrow = null;
+        LastArrow.transform.parent = null;
+        LastArrow.GetComponent<Rigidbody>().isKinematic = false;
+        LastArrow.GetComponent<Rigidbody>().angularVelocity = OVRInput.GetLocalControllerAngularVelocity(controller);
+        LastArrow.GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(controller);
+        hasArrow = false;
+        isAttached = false;
+    }
+
+    private void Fire()
+    {
+        if (isAttached && OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger) < 0)
+        {
+            float dist = (arrowStartPoint.transform.position - controler.transform.position).magnitude;
+            currentArrow.transform.parent = null;
+            Rigidbody r = currentArrow.GetComponent<Rigidbody>();
+            r.velocity = currentArrow.transform.forward * 25f * dist;
+            r.useGravity = true;
+            currentArrow.GetComponent<Collider>().isTrigger = false;
+            currentArrow = null;
+            hasArrow = false;
+            isAttached = false;
+        }
+    }
+
+    public void playSound()
+    {
+
+    }
+
+    public void AttachArrow()
+    {
+        if (currentArrow == null)
+        {
+            currentArrow = Instantiate(arrowPrefab, controler.transform);
+            currentArrow.transform.localPosition = new Vector3(0.1f, 0f, 0.3f);
+            currentArrow.GetComponent<Rigidbody>().isKinematic = true;
+            hasArrow = true;
+        }
+    }
+
+    public void AttachBowToArrow()
+    {
+        currentArrow.transform.parent = controler.transform;
+        currentArrow.transform.position = arrowStartPoint.transform.position;
+        currentArrow.transform.rotation = arrowStartPoint.transform.rotation;
+
+        isAttached = true;
+    }
+}
